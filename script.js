634da978d97a50a65b935524640360dd7e926daf
Dukelucasleon <157530764+Dukelucasleon@users.noreply.github.com>
2025-12-02 10:07:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ function processGraph(maxYValue) {
   const data = imageData.data;
   const bars = [];
 
-  const minWidth = Math.floor(canvas.width * 0.07); // 7% of width
+  const minWidth = Math.floor(canvas.width * 0.05); // 5% of width
   const minHeight = 20;
 
   let currentBar = null;
